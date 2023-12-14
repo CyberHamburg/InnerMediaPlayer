aa37161f65a580a70a68bb435f9ad4c24ef48aa0
milkare <dark9spring@gmail.com>
2023-12-14 11:15:15 +0800

    引用PlayingList使其减少和其他UI类耦合

--- Assets/Scripts/UI/PlayList.cs
+++ Assets/Scripts/UI/PlayList.cs
@@ -1,5 +1,14 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using InnerMediaPlayer.Base;
+using InnerMediaPlayer.Logical;
+using InnerMediaPlayer.Models.Signal;
+using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
+
+#pragma warning disable IDE0051
 
 namespace InnerMediaPlayer.UI
 {
@@ -7,13 +16,24 @@ namespace InnerMediaPlayer.UI
     {
         private Button _return;
         private ScrollRect _scrollRect;
+        private PlayingList _playingList;
 
         internal ScrollRect ScrollRect
         {
             get
             {
-                return _scrollRect ??= FindGameObjectInList("List", null).GetComponent<ScrollRect>();
+                return _scrollRect = _scrollRect != null ? _scrollRect : FindGameObjectInList("List", null).GetComponent<ScrollRect>();
             }
+        }
+
+        internal bool Pause => _playingList.Pause;
+        internal float CurrentTime => _playingList.CurrentTime;
+        internal float TotalTime => _playingList.TotalTime;
+        internal float? AlreadyPlayedRate => _playingList.AlreadyPlayedRate;
+        [Inject]
+        private void Initialized(PlayingList playingList)
+        {
+            _playingList = playingList;
         }
 
         private void Start()
@@ -32,6 +52,29 @@ namespace InnerMediaPlayer.UI
         {
             gameObject.SetActive(false);
         }
-    }
 
+        internal bool? PlayOrPause() => _playingList.PlayOrPause();
+
+        internal void Next() => _playingList.Next();
+
+        internal void Previous() => _playingList.Previous();
+
+        internal void ProcessAdjustment(float value) => _playingList.ProcessAdjustment(value);
+
+        internal Task IterationListAsync(Action<PlayingList.Song> updateUI, Action<int> disposeLyric,
+            Action<int> disableLyric, int disposedSongId, bool stopByForce, LyricDisplaySignal lyricDisplaySignal,
+            CancellationToken token) =>
+            _playingList.IterationListAsync(updateUI, disposeLyric, disableLyric, disposedSongId, stopByForce,
+                lyricDisplaySignal, token);
+
+        internal Task<AudioClip> GetAudioClipAsync(int id) => _playingList.GetAudioClipAsync(id);
+
+        internal int ForceAdd(int id, string songName, string artist, AudioClip audioClip, Sprite album,
+            RectTransform uiContent, Action<int> disposeLyric) =>
+            _playingList.ForceAdd(id, songName, artist, audioClip, album, uiContent, disposeLyric);
+
+        internal void AddToList(int id, string songName, string artist, AudioClip audioClip, Sprite album,
+            RectTransform uiContent, Action<int> disposeLyric) =>
+            _playingList.AddToList(id, songName, artist, audioClip, album, uiContent, disposeLyric);
+    }
 }
