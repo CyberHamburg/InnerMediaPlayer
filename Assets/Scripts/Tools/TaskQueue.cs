ce07fca94bcbe195f556dfd2dd3645e4e237e45c
milkare <dark9spring@gmail.com>
2023-12-14 10:55:54 +0800

    增加手动停止和信号绑定方法

--- Assets/Scripts/Tools/TaskQueue.cs
+++ Assets/Scripts/Tools/TaskQueue.cs
@@ -2,17 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using InnerMediaPlayer.Models;
+using UnityEngine;
 
 namespace InnerMediaPlayer.Tools
 {
+    /// <summary>
+    /// 同时只有一个任务在运行的队列，再次加入任务会取消前面正在运行的任务
+    /// </summary>
     internal class TaskQueue
     {
         private readonly Queue<Func<CancellationToken, Task>> _taskQueue;
         protected Task currentTask;
         protected CancellationTokenSource cancelTokenSource;
-        protected bool isRunning;
+        protected bool isRunningRunMethod;
 
-        protected TaskQueue()
+        internal TaskQueue()
         {
             _taskQueue = new Queue<Func<CancellationToken, Task>>(3);
         }
@@ -25,9 +30,9 @@ namespace InnerMediaPlayer.Tools
 
         private async void Run()
         {
-            while (_taskQueue.Count > 0 && !isRunning)
+            while (_taskQueue.Count > 0 && !isRunningRunMethod)
             {
-                isRunning = true;
+                isRunningRunMethod = true;
                 Func<CancellationToken, Task> newTaskFunc = _taskQueue.Dequeue();
                 if (currentTask != null && !currentTask.IsCompleted)
                 {
@@ -40,16 +45,36 @@ namespace InnerMediaPlayer.Tools
                 cancelTokenSource = new CancellationTokenSource();
                 currentTask = newTaskFunc(cancelTokenSource.Token);
                 await Task.Yield();
-                isRunning = false;
+                isRunningRunMethod = false;
             }
+        }
+
+        protected internal void Stop()
+        {
+            if (cancelTokenSource == null) 
+                return;
+#if UNITY_DEBUG
+            Debug.Log("停止任务运行");
+#endif
+            cancelTokenSource.Cancel();
+            cancelTokenSource = null;
+        }
+
+        internal void Binder<T>(T t) where T : ITaskQueueBinding
+        {
+            t.CallBack?.Invoke();
+            AddTask(t.Func);
         }
     }
 
+    /// <summary>
+    /// 同时只有一个任务在运行的队列，再次加入任务会取消前面正在运行的任务
+    /// </summary>
     internal class TaskQueue<TParam1> : TaskQueue
     {
         private readonly Queue<Func<TParam1, CancellationToken, Task>> _taskQueue;
 
-        protected TaskQueue()
+        internal TaskQueue()
         {
             _taskQueue = new Queue<Func<TParam1, CancellationToken, Task>>(3);
         }
@@ -62,9 +87,9 @@ namespace InnerMediaPlayer.Tools
 
         private async void Run(TParam1 param1)
         {
-            while (_taskQueue.Count > 0 && !isRunning)
+            while (_taskQueue.Count > 0 && !isRunningRunMethod)
             {
-                isRunning = true;
+                isRunningRunMethod = true;
                 Func<TParam1, CancellationToken, Task> newTaskFunc = _taskQueue.Dequeue();
                 if (currentTask != null && !currentTask.IsCompleted)
                 {
@@ -77,16 +102,25 @@ namespace InnerMediaPlayer.Tools
                 cancelTokenSource = new CancellationTokenSource();
                 currentTask = newTaskFunc(param1, cancelTokenSource.Token);
                 await Task.Yield();
-                isRunning = false;
+                isRunningRunMethod = false;
             }
+        }
+
+        internal new void Binder<T>(T t) where T : ITaskQueueBinding<TParam1>
+        {
+            t.CallBack?.Invoke();
+            AddTask(t.Param1, t.Func);
         }
     }
 
+    /// <summary>
+    /// 同时只有一个任务在运行的队列，再次加入任务会取消前面正在运行的任务
+    /// </summary>
     internal class TaskQueue<TParam1, TParam2> : TaskQueue<TParam1>
     {
         private readonly Queue<Func<TParam1, TParam2, CancellationToken, Task>> _taskQueue;
 
-        public TaskQueue()
+        internal TaskQueue()
         {
             _taskQueue = new Queue<Func<TParam1, TParam2, CancellationToken, Task>>(3);
         }
@@ -99,9 +133,9 @@ namespace InnerMediaPlayer.Tools
 
         private async void Run(TParam1 param1, TParam2 param2)
         {
-            while (_taskQueue.Count > 0 && !isRunning)
+            while (_taskQueue.Count > 0 && !isRunningRunMethod)
             {
-                isRunning = true;
+                isRunningRunMethod = true;
                 Func<TParam1, TParam2, CancellationToken, Task> newTaskFunc = _taskQueue.Dequeue();
                 if (currentTask != null && !currentTask.IsCompleted)
                 {
@@ -114,16 +148,25 @@ namespace InnerMediaPlayer.Tools
                 cancelTokenSource = new CancellationTokenSource();
                 currentTask = newTaskFunc(param1, param2, cancelTokenSource.Token);
                 await Task.Yield();
-                isRunning = false;
+                isRunningRunMethod = false;
             }
+        }
+
+        internal new void Binder<T>(T t) where T : ITaskQueueBinding<TParam1, TParam2>
+        {
+            t.CallBack?.Invoke();
+            AddTask(t.Param1, t.Param2, t.Func);
         }
     }
 
+    /// <summary>
+    /// 同时只有一个任务在运行的队列，再次加入任务会取消前面正在运行的任务
+    /// </summary>
     internal class TaskQueue<TParam1, TParam2, TParam3> : TaskQueue<TParam1, TParam2>
     {
         private readonly Queue<Func<TParam1, TParam2, TParam3, CancellationToken, Task>> _taskQueue;
 
-        public TaskQueue()
+        internal TaskQueue()
         {
             _taskQueue = new Queue<Func<TParam1, TParam2, TParam3, CancellationToken, Task>>(3);
         }
@@ -137,9 +180,9 @@ namespace InnerMediaPlayer.Tools
 
         private async void Run(TParam1 param1, TParam2 param2, TParam3 param3)
         {
-            while (_taskQueue.Count > 0 && !isRunning)
+            while (_taskQueue.Count > 0 && !isRunningRunMethod)
             {
-                isRunning = true;
+                isRunningRunMethod = true;
                 Func<TParam1, TParam2, TParam3, CancellationToken, Task> newTaskFunc = _taskQueue.Dequeue();
                 if (currentTask != null && !currentTask.IsCompleted)
                 {
@@ -152,8 +195,14 @@ namespace InnerMediaPlayer.Tools
                 cancelTokenSource = new CancellationTokenSource();
                 currentTask = newTaskFunc(param1, param2, param3, cancelTokenSource.Token);
                 await Task.Yield();
-                isRunning = false;
+                isRunningRunMethod = false;
             }
+        }
+
+        internal new void Binder<T>(T t) where T : ITaskQueueBinding<TParam1, TParam2, TParam3>
+        {
+            t.CallBack?.Invoke();
+            AddTask(t.Param1, t.Param2, t.Param3, t.Func);
         }
     }
 }
