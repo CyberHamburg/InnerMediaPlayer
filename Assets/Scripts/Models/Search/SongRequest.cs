0c5ebcab52f074a66a2fd516cef24d1beefc43bf
milkare <dark9spring@gmail.com>
2024-06-13 17:50:22 +0800

    更改aac解码类型为mp3

--- Assets/Scripts/Models/Search/SongRequest.cs
+++ Assets/Scripts/Models/Search/SongRequest.cs
@@ -11,14 +11,20 @@ namespace InnerMediaPlayer.Models
                 _ids = $"[{value}]";
             }
         }
+
         public string level { get; set; }
+        /// <summary>
+        /// <para>解码类型,有aac和mp3等</para>
+        /// <para>aac可获得更好的音频质量，但需要对unity不支持的m4a做转码</para>
+        /// <para>mp3得到较低的质量，不用转码</para>
+        /// </summary>
         public string encodeType { get; set; }
         public string csrf_token { get; set; }
 
         public SongRequest(int id, string csrfToken)
         {
             level = "standard";
-            encodeType = "aac";
+            encodeType = "mp3";
             ids = id.ToString();
             csrf_token = csrfToken;
         }
@@ -26,7 +32,7 @@ namespace InnerMediaPlayer.Models
         public SongRequest()
         {
             level = "standard";
-            encodeType = "aac";
+            encodeType = "mp3";
         }
     }
 }
