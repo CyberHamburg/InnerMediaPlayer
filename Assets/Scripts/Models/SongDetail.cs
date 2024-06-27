44bd69939cd43dedb39e20090ecfe0cfc5945bd4
milkare <dark9spring@gmail.com>
2024-06-27 17:51:03 +0800

    新增字段

--- Assets/Scripts/Models/SongDetail.cs
+++ Assets/Scripts/Models/SongDetail.cs
@@ -4,13 +4,14 @@ using UnityEngine.UI;
 namespace InnerMediaPlayer.Models.Search
 {
     /// <summary>
-    /// 储存搜索结果列表界面中的单个元素
+    /// 储存搜索结果列表界面中的所有单个UI元素
     /// </summary>
     internal class SongDetail
     {
         internal GameObject _root;
         internal Button _play;
         internal Button _addList;
+        internal RectTransform _textMask;
         internal Text _songName;
         internal Text _artist;
         internal Image _album;
