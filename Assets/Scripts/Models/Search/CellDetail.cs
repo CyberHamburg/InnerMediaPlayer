addec1632c90768fc2c9deeaed8f6e511bee62c7
milkare <dark9spring@gmail.com>
2024-07-17 10:47:49 +0800

    将展示艺人的项更改为也可展示专辑

--- Assets/Scripts/Models/Search/CellDetail.cs
+++ Assets/Scripts/Models/Search/CellDetail.cs
@@ -6,11 +6,11 @@ namespace InnerMediaPlayer.Models.Search
     /// <summary>
     /// 存放搜索结果列表界面中所有单个UI元素
     /// </summary>
-    internal class ArtistDetail
+    internal class CellDetail
     {
         internal GameObject _root;
-        internal Image _artist;
-        internal Text _artistText;
+        internal Image _image;
+        internal Text _text;
         internal RectTransform _textMask;
         internal Button _click;
     }
---
+++ Assets/Scripts/Models/Search/UIItemConfig.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InnerMediaPlayer.Models.Search
+{
+    [Serializable]
+    internal class UIItemConfig
+    {
+        internal string _requestKeywords;
+        internal float _textOriginalSizeX;
+        [SerializeField]
+        [Range(0, 50)]
+        protected internal int _displayNumPerPage = 15;
+
+        internal CellDetail[] _items;
+        internal List<SongDetail> _songsItems;
+
+        internal RectTransform _resultContainer;
+        internal RectTransform _songContainer;
+
+        internal Button _returnLastPanel;
+    }
+}
